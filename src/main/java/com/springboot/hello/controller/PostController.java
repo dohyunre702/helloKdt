0f7d45d65c78ad7dac8daf7ffd4a323c25ef39dc
dohyu <dohyunre702@gmail.com>
2022-10-27 14:21:58 +0900

    실습 4-2. 2. post api 생성 - Map, 람다 활용

--- src/main/java/com/springboot/hello/controller/PostController.java
+++ src/main/java/com/springboot/hello/controller/PostController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/post-api")
 public class PostController {
@@ -17,11 +19,19 @@ public class PostController {
         }
 
     //실습 4-2. Map 활용
+    @RequestMapping(value = "/domain1", method = RequestMethod.POST)
+    public String postRequestParam1(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
 
+        postData.entrySet().forEach((map) -> {
+            sb.append(map.getKey() + ":" + map.getValue() + "\n");
+        });
+        return sb.toString();
+    }
 
     //실습 4-1. post api_memberDto 객체 가져오기
     @RequestMapping(value = "/domain2", method = RequestMethod.POST)
-    public String postRequestParam(@RequestBody MemberDto memberDto) {
+    public String postRequestParam2(@RequestBody MemberDto memberDto) {
         return memberDto.toString();
     }
     //RequestBody : Body 내용을 지정된 객체에 매핑
