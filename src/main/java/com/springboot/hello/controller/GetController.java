91df206e4bfcfad35c10a29d7776f1c3f614b606
dohyu <dohyunre702@gmail.com>
2022-10-28 11:35:53 +0900

    실습 7. GetController logging 적용 1. @Slf4j Annotation class 위에 추가 2. log.info() 추가

--- src/main/java/com/springboot/hello/controller/GetController.java
+++ src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,6 +1,7 @@
 package com.springboot.hello.controller;
 
 import domain.Dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Member;
@@ -8,10 +9,12 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getHello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
 
@@ -24,18 +27,22 @@ public class GetController {
     //(@PathVariable)매개변수 활용한 구현
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable(@PathVariable String variable) {
+
+        log.info("variable1로 요청이 들어왔습니다.");
         return variable;
     }
 
     //실습1.
     @GetMapping(value = "/request1") //request1을 받는 엔드포인트 만들기
     public String getVariable1(@RequestParam String name, @RequestParam String email, @RequestParam String organization) {//requestParam으로 값 3개
+        log.info("request1로 요청이 들어왔습니다.");
         return String.format("%s %s %s", name, email, organization); //받은 값 그대로 리턴
     }
 
     //실습2.
     @GetMapping(value = "/request2")
     public String getVariable2(@RequestParam Map<String, String> param){
+        log.info("request2로 요청이 들어왔습니다.");
         //람다식으로 Map 전체 출력하기
         param.forEach((key, value) -> {
             System.out.println("[key]:" + key + ", [value]:" + value);
@@ -46,6 +53,7 @@ public class GetController {
     //실습3. Dto객체 활용한 GET요청 생성
     @GetMapping(value = "/request3")
     public String getRequestParam(MemberDto memberDto) { //MemberDto 참조변수 선언
+        log.info("request3로 요청이 들어왔습니다.");
         return memberDto.toString(); //memberDto의 toString 메소드
     }
 }
