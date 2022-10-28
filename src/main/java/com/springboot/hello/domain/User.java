4fbbdf34550fef0ba0a08fda31c78bf7b3d473d5
dohyu <dohyunre702@gmail.com>
2022-10-28 14:32:23 +0900

    User VO(Domain) 생성

--- src/main/java/com/springboot/hello/domain/User.java
+++ src/main/java/com/springboot/hello/domain/User.java
@@ -1,5 +1,27 @@
 package com.springboot.hello.domain;
 
+
 public class User {
 
+    private String id;
+    private String name;
+    private String password;
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
