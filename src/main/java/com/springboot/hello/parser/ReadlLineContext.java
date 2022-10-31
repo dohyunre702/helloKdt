9619bf29ff0a4fb366e3af40b5148d11a376e0d3
dohyu <dohyunre702@gmail.com>
2022-10-31 15:16:27 +0900

    Hospital 프로젝트 - ReadLineContext 예외처리 리팩토링

--- src/main/java/com/springboot/hello/parser/ReadlLineContext.java
+++ src/main/java/com/springboot/hello/parser/ReadlLineContext.java
@@ -20,7 +20,11 @@ public class ReadlLineContext<T> {
         );
         String str;
         while((str = reader.readLine()) != null) {
-            result.add(parser.parse(str));
+            try {
+                result.add(parser.parse(str));
+            } catch (Exception e) {
+                System.out.printf("파싱중 문제가 생겨 이 라인은 넘어갑니다. 파일내용: %s", str);
+            }
         }
         reader.close();
         return result;
