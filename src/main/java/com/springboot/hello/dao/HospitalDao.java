3cfb44e9a24753efb5eeef1a6d2536ca853bace9
dohyu <dohyunre702@gmail.com>
2022-11-01 15:46:43 +0900

    HospitalDao 수정 - RowMapper : sql 매핑 완성

--- src/main/java/com/springboot/hello/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/dao/HospitalDao.java
@@ -55,8 +55,21 @@ public class HospitalDao {
         Hospital hospital = new Hospital();
         hospital.setId(rs.getInt("id"));
         hospital.setOpenServiceName(rs.getString("open_service_name"));
-        hospital.setHospitalName(rs.getString("hospital_name"));
+        hospital.setOpenLocalGovernmentCode(rs.getInt("open_local_government_code"));
+        hospital.setManagementNumber(rs.getString(""));
         hospital.setLicenseDate(rs.getTimestamp("license_date").toLocalDateTime());
+        hospital.setBusinessStatus(rs.getInt("business_status"));
+        hospital.setBusinessStatusCode(rs.getInt("business_status_code"));
+        hospital.setPhone(rs.getString("phone"));
+        hospital.setFullAddress(rs.getString("full_address"));
+        hospital.setRoadNameAddress(rs.getString("road_name_address"));
+        hospital.setHospitalName(rs.getString("hospital_name"));
+        hospital.setBusinessTypeName(rs.getString("business_type_name"));
+        hospital.setHealthcareProviderCount(rs.getInt("healthcare_provider_count"));
+        hospital.setPatientRoomCount(rs.getInt("patient_room_count"));
+        hospital.setTotalNumberOfBeds(rs.getInt("total_number_of_beds"));
+        hospital.setTotalAreaSize(rs.getFloat("total_area_size"));
+
         //나머지 완성, 이후 테스트 코드 완성하기
         return hospital;
 
