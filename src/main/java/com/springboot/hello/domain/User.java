7a86079c2296d3ad0693ce80266b2753ed0db703
dohyu <dohyunre702@gmail.com>
2022-11-03 11:46:29 +0900

    Hospital domain.user에 롬복 적용

--- src/main/java/com/springboot/hello/domain/User.java
+++ src/main/java/com/springboot/hello/domain/User.java
@@ -1,27 +1,15 @@
 package com.springboot.hello.domain;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class User {
     private String id;
     private String name;
     private String password;
-
-    public User(String id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
 }
