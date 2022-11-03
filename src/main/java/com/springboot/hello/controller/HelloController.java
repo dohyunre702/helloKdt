695b6868e6a854bd50e0fda0c3360d95cf46c769
dohyu <dohyunre702@gmail.com>
2022-11-03 13:20:08 +0900

    로그 남기기 예제

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -1,14 +1,27 @@
 package com.springboot.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/get-api")
+@Slf4j //로그 남기기
 public class HelloController {
-
-    @RequestMapping("/hello") //controller에 어떤 메소드를 할당할지 정해줌
+    @RequestMapping("/hello", method = RequestMethod.GET) //controller에 어떤 메소드를 할당할지 정해줌
     public String hello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello world";
     }
 
+    @GetMapping(value = "/name")
+    public String getName() {
+        log.info("getName으로 요청이 들어왔습니다.");
+        return "DH";
+    }
+
+    @GetMapping(value="variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        log.info("getVariable1으로 요청이 들어왔습니다. variable:{}", variable);
+        return variable;
+    }
 }
