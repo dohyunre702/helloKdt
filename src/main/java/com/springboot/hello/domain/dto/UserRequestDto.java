8722e6b536cf4b39b2ac7a9face524e2d65af964
dohyu <dohyunre702@gmail.com>
2022-11-03 11:42:41 +0900

    Hospital userRequestDto에 롬복 적용

--- src/main/java/com/springboot/hello/domain/dto/UserRequestDto.java
+++ src/main/java/com/springboot/hello/domain/dto/UserRequestDto.java
@@ -1,27 +1,13 @@
 package com.springboot.hello.domain.dto;
 
-import com.springboot.hello.domain.User;
+//롬복 사용
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public class UserRequestDto {
     private String id;
     private String name;
     private String password;
-
-    public UserRequestDto(String id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
