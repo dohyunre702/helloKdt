1b7abccca1244772824e2722e661d2fb8adb8b33
dohyu <dohyunre702@gmail.com>
2022-11-03 11:51:14 +0900

    Hospital hospitalDao getCount() 리턴값 수정

--- src/main/java/com/springboot/hello/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/dao/HospitalDao.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Date;
+import java.time.LocalDate;
 
 @Component
 public class HospitalDao {
@@ -18,36 +19,6 @@ public class HospitalDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void add(Hospital hospital) {
-        String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals()`" +
-                "(`id`, `open_service_name`, `open_local_government_code`," +
-                "`management_number`, `license_date`, `business_status`," +
-                "`business_status_code`, `phone`, `full_address`," +
-                "`road_name_address`,`hospital_name`,`business_type_name`" +
-                "`healthcare_provider_count`,`patient_room_count`,`total_number_of_beds`" +
-                "`total_area_size`);" +
-                " VALUES (?,?,?," +
-                "?,?,?" +
-                "?,?,?" +
-                "?,?,?" +
-                "?,?,?" +
-                "?);";
-
-        this.jdbcTemplate.update(sql,
-                hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
-                hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(),
-                hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(),
-                hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(),
-                hospital.getHealthcareProviderCount(),hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(),
-                hospital.getTotalAreaSize()
-                );
-    }
-
-    public int getCount() {
-        String sql = "SELECT count(id) from nation_wide_hospitals;";
-        this.jdbcTemplate.queryForObject(sql, Integer.class);
-        return 0;
-    }
 
     RowMapper<Hospital> rowMapper = (rs,rowNum) -> {
         Hospital hospital = new Hospital();
@@ -67,10 +38,37 @@ public class HospitalDao {
         hospital.setPatientRoomCount(rs.getInt("patient_room_count"));
         hospital.setTotalNumberOfBeds(rs.getInt("total_number_of_beds"));
         hospital.setTotalAreaSize(rs.getFloat("total_area_size"));
-
         return hospital;
+    };
 
-        };
+    public void add(Hospital hospital) {
+        String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals` (" +
+                "(`id`, `open_service_name`, `open_local_government_code`," +
+                "`management_number`, `license_date`, `business_status`," +
+                "`business_status_code`, `phone`, `full_address`," +
+                "`road_name_address`,`hospital_name`,`business_type_name`" +
+                "`healthcare_provider_count`,`patient_room_count`,`total_number_of_beds`" +
+                "`total_area_size`);" +
+                " VALUES (?,?,?," +
+                "?,?,?" +
+                "?,?,?" +
+                "?,?,?" +
+                "?,?,?" +
+                "?);";
+        this.jdbcTemplate.update(sql,
+                hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
+                hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(),
+                hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(),
+                hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(),
+                hospital.getHealthcareProviderCount(),hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(),
+                hospital.getTotalAreaSize()
+                );
+    }
+
+    public int getCount() {
+        String sql = "SELECT count(id) from nation_wide_hospitals;";
+        return this.jdbcTemplate.queryForObject(sql, Integer.class);
+    }
 
     public Hospital findById(int id) {
         return this.jdbcTemplate.queryForObject("SELECT * FROM nation_wide_hospitals where id = ?", rowMapper,id);
